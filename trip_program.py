0f86f185eb4fc181c785af9f348b0a52af830ea1
Jacob Meadows <2178@chaparralstaracademy.com>
2018-10-24 23:20:07 -0500

    V1.0.2 (Unfinished)
    Unfinished 1.0.2 update.

--- trip_program.py
+++ trip_program.py
@@ -19,16 +19,18 @@
 """
 import tkinter as tk
 from tkinter import ttk
+import math
 
 
 class App(tk.Frame):
     def __init__(self, master):
         self.master = master
         self.master.config(width=390, height=400)
+        self.current_width, self.current_height = 390, 400
         self.master.minsize(width=390, height=400)
         self.master.title("Travel")
         super().__init__(self.master, width=390, height=400)
-        self.place(x=0, y=0)
+        self.place(relx=.5, rely=.5, anchor="center")
 
         try:
             self.travel_log_txt = open("travel_log.txt", "r")
@@ -47,6 +49,8 @@ class App(tk.Frame):
 
         self.widgets = dict()
         self.widget_init()
+
+        # self.window_update()
 
     def widget_init(self):
         self.widgets["menu"] = tk.Menu(self)
@@ -72,20 +76,20 @@ class App(tk.Frame):
         self.widgets["months_spinbox"] = tk.Spinbox(self, values=self.months, state="readonly")
         self.widgets["months_label"].place(x=210 + self.widgets["months_spinbox"]["width"]*2, y=5)
         self.widgets["months_spinbox"].place(x=210, y=30)
-        self.widgets["description_label"] = tk.Label(self, text="Description")
-        self.widgets["description_label"].place(x=10, y=200)
-        self.widgets["description_text"] = tk.Text(self, width=42, wrap="word")
-        self.widgets["description_text"].place(x=10, y=230)
         self.widgets["travel_label"] = tk.Label(self, text="Method of Travel")
         self.widgets["travel_combobox"] = tk.ttk.Combobox(self, values="Air Train Car", state="readonly")
         self.widgets["travel_label"].place(x=190 + self.widgets["travel_combobox"]["width"]*2, y=60)
         self.widgets["travel_combobox"].place(x=210, y=90)
+        self.widgets["description_label"] = tk.Label(self, text="Description")
+        self.widgets["description_label"].place(x=10, y=200)
+        self.widgets["description_text"] = tk.Text(self, width=42, wrap="word")
+        self.widgets["description_text"].place(x=10, y=230)
         self.widgets["submit_button"] = tk.Button(self, text="Submit", command=self.submit_command)
         self.widgets["submit_button"].place(x=210, y=120)
         self.widgets["clear_button"] = tk.Button(self, text="Clear", command=self.clear_command)
         self.widgets["clear_button"].place(x=210, y=150)
-        self.widgets["grip"] = tk.ttk.Sizegrip(self)
-        self.widgets["grip"].place(x=self.config("width")[-1]-15, y=self.config("height")[-1]-15)
+        self.widgets["grip"] = tk.ttk.Sizegrip(self.master)
+        self.widgets["grip"].place(relx=1, rely=1, anchor="se")
 
     def submit_command(self):
         if self.widgets["countries_listbox"].curselection() != () and self.widgets["travel_combobox"].get() != "":
@@ -100,8 +104,29 @@ class App(tk.Frame):
             self.travel_log_txt.close()
             self.travel_log_txt = open("travel_log.txt", "r")
             self.travel_log_info = self.travel_log_txt.read()
-            print(self.travel_log_info)
             self.travel_log_txt.close()
+            self.widgets["submit_toplevel"] = tk.Toplevel(self)
+            self.widgets["submit_toplevel"].title("Success!")
+            self.widgets["submit_toplevel"].focus_set()
+            self.widgets["submit_message"] = tk.Message(self.widgets["submit_toplevel"], width=250,
+                                                        text="Your submission has been saved and properly documented.")
+            self.widgets["submit_message"].pack()
+            self.widgets["okay_button"] = tk.Button(self.widgets["submit_toplevel"], text="Okay",
+                                                    command=self.widgets["submit_toplevel"].destroy)
+            self.widgets["okay_button"].pack(pady=5)
+        else:
+            self.widgets["error_toplevel"] = tk.Toplevel(self)
+            self.widgets["error_toplevel"].title("An Error Has Occurred")
+            self.widgets["error_toplevel"].focus_set()
+            self.widgets["error_message"] = tk.Message(
+                self.widgets["error_toplevel"], width=320,
+                text="Your submission was either incomplete or an error occurred during translation.\n\nPlease fix any "
+                     "errors if applicable, and try again.", justify="center"
+            )
+            self.widgets["error_message"].pack()
+            self.widgets["okay_button"] = tk.Button(self.widgets["error_toplevel"], text="Okay",
+                                                    command=self.widgets["error_toplevel"].destroy)
+            self.widgets["okay_button"].pack(pady=5)
 
     def clear_command(self):
         for widget in self.winfo_children():
@@ -113,13 +138,30 @@ class App(tk.Frame):
     def about_command(self):
         self.widgets["about_toplevel"] = tk.Toplevel(self, )
         self.widgets["about_toplevel"].title("About")
-        self.widgets["about_message"] = tk.Message(self.widgets["about_toplevel"], width=300,
-                                                   text="Author: Jacob Meadows\n"
-                                                        "Version: 1.0.1\n"
-                                                        "Description: A short program that logs inputted data from the "
-                                                        "user based on the survey and any information given in the "
-                                                        "description.")
+        self.widgets["about_message"] = tk.Message(
+            self.widgets["about_toplevel"], width=300,
+            text="Author: Jacob Meadows\nVersion: 1.0.1\nDescription: A short program that logs inputted data from the "
+                 "user based on the survey and any information given in the description."
+        )
         self.widgets["about_message"].pack()
+
+    def window_update(self):
+        # if [str(self.current_width), str(self.current_height)] != self.master.winfo_geometry().split("+")[0].split("x"):
+        #    width, height = [int(x) for x in self.master.winfo_geometry().split("+")[0].split("x")]
+        #    x_difference, y_difference = width - self.current_width, height - self.current_height
+        #    self.place(x=self.winfo_x() + x_difference, y=self.winfo_y() + y_difference)
+        #    self.current_width, self.current_height = width, height
+        # if self.winfo_geometry().split("+")[0] != self.master.winfo_geometry().split("+")[0]:
+        #     new_width, new_height = self.master.winfo_geometry().split("+")[0].split("x")
+        #     width, height = self.winfo_geometry().split("+")[0].split("x")
+        #     width_difference, height_difference = int(new_width) - int(width), int(new_height) - int(height)
+        #     self.config(width=new_width, height=new_height)
+        #     for widget in self.widgets.values():
+        #         if not isinstance(widget, tk.Menu) and not isinstance(widget, tk.StringVar) \
+        #                 and not isinstance(widget, tk.Scrollbar):
+        #             widget.place(x=widget.winfo_x() + width_difference, y=widget.winfo_y() + height_difference)
+        # self.after(1, self.window_update)
+        pass
 
 
 if __name__ == "__main__":
